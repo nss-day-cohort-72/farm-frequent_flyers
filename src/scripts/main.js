6155281cfa4cf3bf98e3390c8726d8e9f97e2eee
alwright33 <austinlucaswright@gmail.com>
2024-08-07 14:15:59 -0500

    built createAsparagus function that returns asparagus object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,8 @@
 import { createPlan } from "./plan.js";
+import { createAsparagus } from "./seeds/asparagus.js";
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
 
 const yearlyPlan = createPlan()
 console.log("Welcome to the main module")
