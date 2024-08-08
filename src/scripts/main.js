1aa1bcd1592d43a004394393ff0343f0dfa7552e
alwright33 <austinlucaswright@gmail.com>
2024-08-08 11:34:58 -0500

    added working plantSeeds funtion that returns seeds array

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,9 @@ import { usePlant, addPlant } from "./field.js";
 import { createPlan } from "./plan.js";
 import { createAsparagus } from "./seeds/asparagus.js";
 import { createPotato } from "./seeds/potato.js";
+import { plantSeeds } from "./tractor.js";
 
 
-//yearlyPlan = createPlan()
-let asparagus = {type: 'Asparagus',
-    height: 24,
-    output: 4    
-}
-addPlant(asparagus)
-
-const plant = usePlant()
-console.log(plant)
+const yearlyPlan = createPlan()
+const seedPlants = plantSeeds(yearlyPlan)
+console.log(seedPlants)
